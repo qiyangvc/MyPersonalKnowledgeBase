9d4da003c0108b779acb1ead1f4959f51f0c1330
Li Guangli <3296702864@qq.com>
2025-05-18 21:01:38 +0800

    后端初始环境配置

---
+++ KnowChain/src/main/java/knowchain/server/KnowChainApplication.java
@@ -0,0 +1,13 @@
+package knowchain.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowChainApplication.class, args);
+    }
+
+}
